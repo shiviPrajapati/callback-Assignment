a30dd8ffb565eaf5aa0e7758e4b7f49be50099fd
shivi <100689315+shiviPrajapati@users.noreply.github.com>
2022-04-06 01:03:11 +0530

    remove promises and await

--- test/testProblem1.js
+++ test/testProblem1.js
@@ -1,4 +1,15 @@
-const problem1 = require ('./../problem1.js');
+const test = require ('../problem1.js');
+
+function fileCreateAndDelete(path1, data){
+    test.createRandomDirectory ('./dirForRandomJSON1', () => {
+        test.createJsonFiles(path1, data, () => {
+           console.log("file created");
+            test.deleteFile(path1, () => {
+                console.log("file deleted!")
+            })
+        })
+   }) 
+}
 
 
-    problem1('./dirForRandomJSON/ra.json' , "hello");
+fileCreateAndDelete('./dirForRandomJSON1/ra.json', "welcome to callbacks")
